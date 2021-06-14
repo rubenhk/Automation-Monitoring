22fdccd240bc7873de656dcd1d8f13ac6333e93a
rubenhk <78378888+rubenhk@users.noreply.github.com>
2021-06-14 09:23:39 +0100

    Create loganalytics_output.tf

---
+++ loganalytics_output.tf
@@ -0,0 +1,15 @@
+output "id" {
+  description = "Output the object ID"
+  value       = azurerm_log_analytics_workspace.log_analytics.id
+}
+
+output "name" {
+  description = "Output the object name"
+  value       = azurerm_log_analytics_workspace.log_analytics.name
+}
+
+output "object" {
+  sensitive   = true
+  description = "Output the full object"
+  value       = azurerm_log_analytics_workspace.log_analytics
+}
