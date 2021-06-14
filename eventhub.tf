f2cba2b601de603cce22af76b92dfde67c131907
rubenhk <78378888+rubenhk@users.noreply.github.com>
2021-06-14 09:12:45 +0100

    Create eventhub.tf

---
+++ eventhub.tf
@@ -0,0 +1,10 @@
+/*
+module "eventhub" {
+  source             = "../modules/eventhub"
+  rg_name            = azurerm_resource_group.monitoring.name
+  location           = azurerm_resource_group.monitoring.location
+  eventhub_namespace = var.eventhub_namespace
+  eventhub_name      = var.eventhub_name
+  tags               = var.eventhub_tags
+}
+*/
